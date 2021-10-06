d9bb394d2766420ae459ea48c03ccd5dac85e36d
Scyruz <a01561869@itesm.mx>
2021-10-05 23:07:52 -0600

    Challenge 4

--- src/components/ProductInfo/index.tsx
+++ src/components/ProductInfo/index.tsx
@@ -8,7 +8,12 @@ import {
   Select,
   MenuItem,
   Button,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
 } from "@material-ui/core";
+import React from "react";
 
 interface ProductInfoProps {
   product: Product;
@@ -31,6 +36,7 @@ const ProductInfo: React.FC<ProductInfoProps> = (props) => {
 
   var colors: any[] = [];
   var sizes: any[] = [];
+  var comments: any[] = [];
 
   var selectedColor = "";
   var selectedSize = "";
@@ -43,8 +49,29 @@ const ProductInfo: React.FC<ProductInfoProps> = (props) => {
     selectedSize = props.product.childSkus[0].size;
     props.product.childSkus.forEach((sku) => {
       sizes.push(<MenuItem value={sku.size}>{sku.size}</MenuItem>);
+    }); 
+
+    props.product.comments.forEach((comment) => {
+      comments.push(
+        <React.Fragment>
+          <ListItem alignItems="flex-start">
+            <ListItemText
+              primary={comment.author}
+              secondary={
+                <React.Fragment>
+                  <Typography>{comment.body}</Typography>
+                  {comment.created}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </React.Fragment>
+      );
     });
   }
+
+  
 
   var largeImageUrl = "";
   if (
@@ -125,6 +152,15 @@ const ProductInfo: React.FC<ProductInfoProps> = (props) => {
           </Grid>
 
           <Grid item lg={12} />
+        </Grid>
+      </Grid>
+      <Grid container className="comments-grid" spacing={2}>
+        <Grid item lg={4}>
+          <Typography className="comments-title" variant="h2" >Comments</Typography>
+        </Grid>
+        <Grid item lg={8} />
+        <Grid item lg={12}>
+          <List>{comments}</List>
         </Grid>
       </Grid>
     </div>
